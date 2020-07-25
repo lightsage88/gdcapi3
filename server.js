d01e8b8f1b46301f706601119dcd958c1555f8fb
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-07-24 18:19:52 -0700

    server tweak

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ function closeServer() {
 }
 
 if(require.main === module) {
-  runServer(`${process.env.DATABASE_URL}`).catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = {app, runServer, closeServer}
